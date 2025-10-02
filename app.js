444dfb2ff52f70381b3326142b3e20d1fe08e7ee
xpz19 <mjcth70@gmail.com>
2025-10-02 17:38:43 +0700

    new
    -เปลี่ยนสีพื้นหลัง
    -เปลี่ยนปก
    -เพิ่มโมเดล

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-function coppyText(){
-  const nameXbox = "Xpz19"
-  navigator.clipboard.writeText(nameXbox);
-  alert("Coppy Name Xbox finish!")
+const modal = document.getElementById("myModal");
+const closeBtn = document.getElementById("closeModal");
+const modalResult = document.getElementById("modalResult");
+const modalCopy = document.getElementById("copy");
+
+
+function xbox() {
+  modal.style.display = "flex"; // แสดง Modal
+  modalResult.innerHTML = ""
 }
 
+function coppyText() {
+  const nameXbox = "Xpz19"
+  navigator.clipboard.writeText(nameXbox);
+  modalResult.innerHTML = `คัดลอกชื่อ Xbox เรียบร้อย`
+  modalCopy.innerHTML = nameXbox
+  modalCopy.style.background = "#3ffa7167"
+}
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
